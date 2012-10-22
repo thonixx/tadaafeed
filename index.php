a3fbceec21c1455db0b740220f46b6062cd6e36c
Michael Tanner <git@pixelwolf.ch>
2012-10-22 14:15:03 +0200

    tested release

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	// scripted by Michael Tanner
 	//
 	// NOTES
-	// - more features may be added later (only if needed to manipulate more things on tadaa feed
+	// - more features may be added later (only if needed to manipulate more things on tadaa feed)
 	// 
 	//
 	// https://github.com/thonixx/tadaafeed
@@ -15,15 +15,20 @@
         //////////////////////////////////////
     
     
+	// user variable
+	$user = $_GET['user'];
+	
+	// set to user thonixx (me) if no user provided
+	if(empty($user)) $user = 'thonixx';
 	
 	//////////////////////////////
 	////// EDIT THIS TO YOUR NEEDS
 	// where to save the final rss
-	$rssfile = $_GET['user'].'.rss';
+	$rssfile = $user.'.rss';
 	///////////////////////////////
-	
+
 	// set right header (content type things)
-	header("Content-Type: application/rss+xml; charset=utf-8");
+	// header("Content-Type: application/rss+xml; charset=utf-8");
 	
 	// twitter search api url
 	$tadaa_url = 'http://www.tadaa.net/'.$rssfile;
@@ -33,8 +38,14 @@
 	// replace m800le with m1200le
 	// for higher resolution in tadaa's rss feed
 	$output = str_replace('m800le', 'm1200le', $tadaa);
-	
+	// also replace annoying special char (which is not really ASCII)
+	$output = str_replace('➹', 'Location: ', $output);
+
 	// now write the stuff to the rss feed
-	file_put_contents($rssfile, $tadaa)
+	// file_put_contents($output, $tadaa); // disabled due to insufficient rights
+	echo $output;
+
+	// echo "Your high resolution tadaa feed is placed here:<br />";
+	// echo "<a href=\"$user.rss\">$user's Tadaa feed</a>";
 
 ?>
